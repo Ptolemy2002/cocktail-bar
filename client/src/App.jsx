0881dfca398fa6c4ec14fbc4b13483a0ea7b81c9
Ptolemy Henson <pbhenson@iu.edu>
2023-11-28 15:18:20 -0500

    Included Bootstrap and LESS. Added basic styling.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomePage from 'src/pages/HomePage';
 import NotFoundPage from './pages/NotFound';
 
 function App() {
     return (
-        <div>
-            <header>
-                <h1>Cocktail Bar Client</h1>
-            </header>
-
-            <hr />
-
-            <Router>
+        <Router>
+            <Header title="Home" />
+            <main className="flex-grow-1">
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="*" element={<NotFoundPage />} />
                 </Routes>
-            </Router>
+            </main>
+            <Footer />
+        </Router>
+    );
+}
 
-            <hr />
+function Header(props) {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
-            <footer>
-                <p>
-                    Ptolemy Henson
-                </p>
-            </footer>
-        </div>
+    function itemClass(name) {
+        return currentPath === name ? "nav-item active" : "nav-item";
+    }
+
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link to="#" className="navbar-brand">{props.title}</Link>
+                <button 
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className={itemClass("/")}>
+                            <Link to="/" className='nav-link'>Home <span className="visually-hidden">(current)</span></Link>
+                        </li>
+
+                        <li className={itemClass("/about")}>
+                            <Link to="/about" className='nav-link'>About</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+function Footer(props) {
+    return (
+        <footer className="container-fluid m-0 bg-light">
+            <p>
+                Ptolemy Henson
+            </p>
+        </footer>
     );
 }
 
