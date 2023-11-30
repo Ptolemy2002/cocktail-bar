27af70b9b50da6ac2056f96a6301097f1a390d9c
Ptolemy Henson <pbhenson@iu.edu>
2023-11-29 22:23:15 -0500

    Added endpoints to count values. Also added a function to make a regex accent insensitive.

--- api/lib/mongo.js
+++ api/lib/mongo.js
@@ -4,6 +4,20 @@ const { errorResponse } = require('lib/misc');
 require('models/Recipe');
 const Recipe = mongoose.model('recipes');
 
+function accentInsensitive(string = '') {    
+    return string
+        .replaceAll(/a/g, '(a|á|à|ä|â)')
+        .replaceAll(/A/g, '(A|Á|À|Ä|Â)')
+        .replaceAll(/e/g, '(e|é|è|ë|ê)')
+        .replaceAll(/E/g, '(E|É|È|Ë|Ê)')
+        .replaceAll(/i/g, '(i|í|ì|ï|î)')
+        .replaceAll(/I/g, '(I|Í|Ì|Ï|Î)')
+        .replaceAll(/o/g, '(o|ó|ò|ö|ô)')
+        .replaceAll(/O/g, '(O|Ó|Ò|Ö|Ô)')
+        .replaceAll(/u/g, '(u|ú|ù|ü|û)')
+        .replaceAll(/U/g, '(U|Ú|Ù|Ü|Û)');
+}
+
 function transformQueryCaseInsensitive(query) {
     for (let key in query) {
         let value = query[key];
@@ -15,12 +29,33 @@ function transformQueryCaseInsensitive(query) {
     return query;
 }
 
+function transformQueryAccentInsensitive(query) {
+    for (let key in query) {
+        let value = query[key];
+        if (typeof value === "string") {
+            query[key] = new RegExp(accentInsensitive(value));
+        } else if (value instanceof RegExp) {
+            query[key] = new RegExp(accentInsensitive(value.source), value.flags);
+        }
+    }
+
+    return query;
+}
+
 function transformQueryMatchWhole(query) {
     for (let key in query) {
         let value = query[key];
         if (value instanceof RegExp) {
-            query[key] = new RegExp("^" + value.source + "$", value.flags);
+            value = value.source;
+        } else if (typeof value === "string") {
+            value = value;
+        } else {
+            continue;
         }
+
+        query[key] = new RegExp("^" + value + "$", "i");
+        // Used to prevent the accentInsensitiveRegex from converting another time
+        query[key].source.accentInsensitive = true;
     }
 
     return query;
@@ -36,30 +71,48 @@ function findFunction(fun) {
             query = transformQueryMatchWhole(query);
         }
 
+        if (!args.accentSensitive) {
+            query = transformQueryAccentInsensitive(query);
+        }
+
+        console.log(query);
+
         return await collection[fun](query);
     }
 }
 
 const find = findFunction("find");
 const findOne = findFunction("findOne");
+const countAll = findFunction("countDocuments");
 
 function findAll(collection) {
     return find(collection, {});
 }
 
-function findWhereEqual(collection, key, value, caseInsensitive=false) {
-    const query = {};
-    query[key] = value;
-    
-    return find(collection, query, { caseInsensitive, matchWhole: true });
+function whereEqual(fun) {
+    return (collection, key, value, caseInsensitive=false) => {
+        const query = {};
+        query[key] = value;
+        
+        return fun(collection, query, { caseInsensitive, matchWhole: true });
+    };
 }
 
-function findWhereContains(collection, key, value, caseInsensitive=false) {
-    const query = {};
-    query[key] = value;
-    
-    return find(collection, query, { caseInsensitive, matchWhole: false });
+function whereContains(fun) {
+    return (collection, key, value, caseInsensitive=false) => {
+        const query = {};
+        query[key] = value;
+        
+        return fun(collection, query, { caseInsensitive, matchWhole: false });
+    };
 }
+
+const findWhereEqual = whereEqual(find);
+const findWhereContains = whereContains(find);
+const findOneWhereEqual = whereEqual(findOne);
+const findOneWhereContains = whereContains(findOne);
+const countWhereEqual = whereEqual(countAll);
+const countWhereContains = whereContains(countAll);
 
 async function listDistinct(collection, field) {
     const result = await collection.distinct(field);
@@ -82,13 +135,20 @@ function tryFunc(func) {
 }
 
 module.exports = {
+    "accentInsensitive": accentInsensitive,
     "transformQueryCaseInsensitive": tryFunc(transformQueryCaseInsensitive),
+    "transformQueryAccentInsensitive": tryFunc(transformQueryAccentInsensitive),
     "transformQueryMatchWhole": tryFunc(transformQueryMatchWhole),
     "find": tryFunc(find),
     "findOne": tryFunc(findOne),
     "findAll": tryFunc(findAll),
+    "countAll": tryFunc(countAll),
     "findWhereEqual": tryFunc(findWhereEqual),
     "findWhereContains": tryFunc(findWhereContains),
+    "findOneWhereEqual": tryFunc(findOneWhereEqual),
+    "findOneWhereContains": tryFunc(findOneWhereContains),
+    "countWhereEqual": tryFunc(countWhereEqual),
+    "countWhereContains": tryFunc(countWhereContains),
     "listDistinct": tryFunc(listDistinct),
     "list": tryFunc(list),
     
--- api/routes/api/v1/index.js
+++ api/routes/api/v1/index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const { findAll, findWhereEqual, findWhereContains, Recipe } = require('lib/mongo');
+const { findAll, findWhereEqual, findWhereContains, countAll, countWhereEqual, countWhereContains, Recipe } = require('lib/mongo');
 const { sendResponse, errorResponse } = require('lib/misc');
 
 function extractKey(res, key, docs, distinct=false) {
+	console.log(docs);
+	if (docs._isError) sendResponse(res, docs);
+
 	switch (key) {
 		case "_id":
 		case "name":
 		case "category":
 		case "glass": {
 			const result = docs.map(doc => doc[key]);
+			if (distinct) {
+				return [...new Set(result)];
+			}
+
+			return result;
+		}
+		case "ingredient": {
+			// Get ingredients from all recipes, flatten the array, remove nulls, and return the result
+			let result = docs.map(doc => doc.ingredients.map(ingredient => ingredient.ingredient)).flat().filter(ingredient => !!ingredient);
+
 			if (distinct) {
 				return [...new Set(result)];
 			}
@@ -31,44 +44,73 @@ router.get("/recipes/all", async (req, res) => {
 	sendResponse(res, result);
 });
 
+router.get("/recipes/all/count", async (req, res) => {
+	const result = await countAll(Recipe);
+	sendResponse(res, result);
+});
+
 router.get("/recipes/all/list-:prop", async (req, res) => {
 	const docs = await findAll(Recipe);
 	const result = extractKey(res, req.params.prop, docs);
 	sendResponse(res, result);
 });
 
+router.get("/recipes/all/list-:prop/distinct", async (req, res) => {
+	const docs = await findAll(Recipe);
+	const result = extractKey(res, req.params.prop, docs, true);
+	sendResponse(res, result);
+});
+
 router.get("/recipes/:key-equals/:value", async (req, res) => {
+	if (req.params.key === "ingredient") req.params.key = "ingredients.ingredient";
 	const result = await findWhereEqual(Recipe, req.params.key, req.params.value, true);
 	sendResponse(res, result);
 });
 
 router.get("/recipes/:key-equals/:value/list-:prop", async (req, res) => {
+	if (req.params.key === "ingredient") req.params.key = "ingredients.ingredient";
 	const docs = await findWhereEqual(Recipe, req.params.key, req.params.value, true);
 	const result = extractKey(res, req.params.prop, docs);
 	sendResponse(res, result);
 });
 
 router.get("/recipes/:key-equals/:value/list-:prop/distinct", async (req, res) => {
+	if (req.params.key === "ingredient") req.params.key = "ingredients.ingredient";
 	const docs = await findWhereEqual(Recipe, req.params.key, req.params.value, true);
 	const result = extractKey(res, req.params.prop, docs, true);
 	sendResponse(res, result);
 });
 
+router.get("/recipes/:key-equals/:value/count", async (req, res) => {
+	if (req.params.key === "ingredient") req.params.key = "ingredients.ingredient";
+	const result = await countWhereEqual(Recipe, req.params.key, req.params.value, true);
+	sendResponse(res, result);
+});
+
 router.get("/recipes/:key-contains/:value", async (req, res) => {
+	if (req.params.key === "ingredient") req.params.key = "ingredients.ingredient";
 	const result = await findWhereContains(Recipe, req.params.key, req.params.value, true);
 	sendResponse(res, result);
 });
 
 router.get("/recipes/:key-contains/:value/list-:prop", async (req, res) => {
+	if (req.params.key === "ingredient") req.params.key = "ingredients.ingredient";
 	const docs = await findWhereContains(Recipe, req.params.key, req.params.value, true);
 	const result = extractKey(res, req.params.prop, docs);
 	sendResponse(res, result); 
 });
 
 router.get("/recipes/:key-contains/:value/list-:prop/distinct", async (req, res) => {
+	if (req.params.key === "ingredient") req.params.key = "ingredients.ingredient";
 	const docs = await findWhereContains(Recipe, req.params.key, req.params.value, true);
 	const result = extractKey(res, req.params.prop, docs, true);
 	sendResponse(res, result);
-}); 
+});
+
+router.get("/recipes/:key-contains/:value/count", async (req, res) => {
+	if (req.params.key === "ingredient") req.params.key = "ingredients.ingredient";
+	const result = await countWhereContains(Recipe, req.params.key, req.params.value, true);
+	sendResponse(res, result);
+});
 
 module.exports = router;
